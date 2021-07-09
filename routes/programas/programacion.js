ca2da0676c8113dec80ba2e08047da788d25d29f
Heiker Gil <gilheyker@gmail.com>
2021-07-09 13:21:14 -0500

    programas with hours working

--- routes/programas/programacion.js
+++ routes/programas/programacion.js
@@ -29,26 +29,34 @@ if (fechaBusqueda) {
        fechaAnterior.setHours(fechaAnterior.getHours() -5);
        fechaAnterior.setUTCHours(0,0,0,0);
        fechaAnterior.setHours(fechaAnterior.getHours() - 24);
+       const fechaAnterior_rango = new Date(fechaAnterior)
+        fechaAnterior_rango.setHours(fechaAnterior_rango.getHours() + 23,59,59);
 
         // Fecha Actual
         const fechaAuto = new Date();
         fechaAuto.setHours(fechaAuto.getHours() -5);
         fechaAuto.setUTCHours(0,0,0,0);
+        const fechaAuto_rango = new Date(fechaAuto)
+        fechaAuto_rango.setHours(fechaAuto_rango.getHours() + 23,59,59);
+        
+        
         
         // fecha mañana
        const fechaProxima = new Date();
        fechaProxima.setHours(fechaProxima.getHours() - 5);
        fechaProxima.setUTCHours(0,0,0,0);
        fechaProxima.setHours(fechaProxima.getHours() + 24);
+       const fechaProxima_rango = new Date(fechaProxima)
+        fechaProxima_rango.setHours(fechaProxima_rango.getHours() + 23,59,59);
        
        
         const options = {year: 'numeric', month: 'numeric', day: 'numeric' };
         const fechaIngreso =  fechaAuto.toISOString().split('T')[0];
 
-        const programacionAnterior = await Programa.find({"fecha" : fechaAnterior});
-        const programacion = await Programa.find({"fecha" : fechaAuto});
-        const programacionProxima = await Programa.find({"fecha" : fechaProxima});
-        
+        const programacionAnterior = await Programa.find({"fecha": {$gte:fechaAnterior, $lte: fechaAnterior_rango}});
+        const programacion = await Programa.find({"fecha" : {$gte:fechaAuto, $lte:fechaAuto_rango}});
+        const programacionProxima = await Programa.find({"fecha": {$gte: fechaProxima, $lte: fechaAnterior_rango}});
+        console.log(programacion);
         res.render('programacion', { programacion, programacionAnterior, programacionProxima, fecha: fechaAuto.toDateString('en-GB', options), fechaBusqueda: fechaAuto, fechaIngreso:fechaIngreso});
 }
 }))
@@ -72,6 +80,7 @@ router.post('/programacion', wrapAsync(async (req, res, next) => {
     if (verificarLote) {
          res.send(`Lote ya esta asignado a ${verificarLote.proveedor} piscina: ${verificarLote.piscina}`);
      } else {
+         console.log(nuevoPrograma);
       await nuevoPrograma.save()
       res.redirect('/programacion'); 
      }
